c141404e899888426f7969a5336a2a275703a8e5
Alexander <sd_evolution@yahoo.de>
2018-01-03 16:17:16 +0100

    Fertige Version mit Faktenbasis und Regeln entsprechend der Aufgabenstellung.

--- 4. Übung/bachelor_prufungsordnung.pl
+++ 4. Übung/bachelor_prufungsordnung.pl
@@ -1,63 +1,226 @@
-offen(clara, tgi, ev).
-
+/*FAKTEN:*/
 studienzeit_nicht_ueberschritten(clara).
+studienzeit_nicht_ueberschritten(claus).
+studienzeit_nicht_ueberschritten(clemens).
+studienzeit_nicht_ueberschritten(claudia).
 
-angemeldet(clara,tgi).
-angemeldet(clara,ra).
+eingeschrieben(clara, bachelor_informatik, tu_dresden).
+eingeschrieben(claus, bachelor_informatik, tu_dresden).
+eingeschrieben(clemens, bachelor_informatik, tu_dresden).
+eingeschrieben(claudia, bachelor_informatik, tu_dresden).
 
-allg_zugelassen(clara).
+erklaerung(clara).
+erklaerung(claus).
+erklaerung(clemens).
+erklaerung(claudia).
 
-abgelegt(tgi, clara, 4.0).
-abgelegt(ra, clara, 4.0).
-abgelegt(mathe, clara, 3.9).
+angemeldet(claus,mathematische_methoden_fuer_informatiker).
+angemeldet(claus,programmierung).
+angemeldet(claus,informtaions_und_kodierungstheorie).
+angemeldet(claus, softwaretechnologie).
 
-bachelorarbeit(clara, 4.0).
-kolloquium(clara, 4.0).
+offen(claus, mathematische_methoden_fuer_informatiker, erst_pruefung).
+offen(claus, programmierung, erste_wiederholung).
+offen(claus, informtaions_und_kodierungstheorie, zweite_wiederholung).
+offen(claus, formale_systeme, freiversuch).
+offen(claus, theoretische_informatik_und_logik, freiversuch).
+offen(claus, softwaretechnologie, erst_pruefung).
+offen(claus, softwaretechnologie_projekt, erst_pruefung).
+offen(claus, rechnerarchitektur, freiversuch).
+offen(claus, datenbanken_und_rechnernetze, freiversuch).
+offen(claus, betriebssysteme_und_sicherheit, freiversuch).
+offen(claus, technische_grundlagen_und_hardware_praktikum, freiversuch).
+offen(claus, systemorientierte_informatik_und_hardware_software_codedesign, freiversuch).
+offen(claus, intelligente_systeme, freiversuch).
+offen(claus, einfuehrung_in_die_computergraphik, erst_pruefung).
+offen(claus, vertiefung, erst_pruefung).
+offen(claus, vertiefung_zur_bachelor_arbeit, erst_pruefung).
+offen(claus, algemeine_qualifikationen, erst_pruefung).
+offen(claus, mathe1, erst_pruefung).
+offen(claus, mathe2, zweite_wiederholung).
+
+offen(clemens, mathematische_methoden_fuer_informatiker, erst_pruefung).
+offen(clemens, programmierung, erste_wiederholung).
+offen(clemens, informtaions_und_kodierungstheorie, zweite_wiederholung).
+offen(clemens, formale_systeme, freiversuch).
+offen(clemens, theoretische_informatik_und_logik, freiversuch).
+offen(clemens, softwaretechnologie, erst_pruefung).
+offen(clemens, softwaretechnologie_projekt, erst_pruefung).
+offen(clemens, rechnerarchitektur, freiversuch).
+offen(clemens, datenbanken_und_rechnernetze, freiversuch).
+offen(clemens, betriebssysteme_und_sicherheit, freiversuch).
+offen(clemens, technische_grundlagen_und_hardware_praktikum, freiversuch).
+offen(clemens, systemorientierte_informatik_und_hardware_software_codedesign, freiversuch).
+offen(clemens, intelligente_systeme, freiversuch).
+offen(clemens, einfuehrung_in_die_computergraphik, erst_pruefung).
+offen(clemens, vertiefung, erst_pruefung).
+offen(clemens, vertiefung_zur_bachelor_arbeit, erst_pruefung).
+offen(clemens, algemeine_qualifikationen, erst_pruefung).
+
+offen(claudia, mathematische_methoden_fuer_informatiker, erst_pruefung).
+offen(claudia, programmierung, erste_wiederholung).
+offen(claudia, informtaions_und_kodierungstheorie, zweite_wiederholung).
+offen(claudia, formale_systeme, freiversuch).
+offen(claudia, theoretische_informatik_und_logik, freiversuch).
+offen(claudia, softwaretechnologie, erst_pruefung).
+offen(claudia, softwaretechnologie_projekt, erst_pruefung).
+offen(claudia, rechnerarchitektur, freiversuch).
+offen(claudia, datenbanken_und_rechnernetze, freiversuch).
+offen(claudia, betriebssysteme_und_sicherheit, freiversuch).
+offen(claudia, technische_grundlagen_und_hardware_praktikum, freiversuch).
+offen(claudia, systemorientierte_informatik_und_hardware_software_codedesign, freiversuch).
+offen(claudia, intelligente_systeme, freiversuch).
+offen(claudia, einfuehrung_in_die_computergraphik, erst_pruefung).
+offen(claudia, vertiefung, erst_pruefung).
+offen(claudia, vertiefung_zur_bachelor_arbeit, erst_pruefung).
+offen(claudia, algemeine_qualifikationen, erst_pruefung).
+offen(claudia, mathe1, zweite_wiederholung).
+offen(claudia, mathe2, zweite_wiederholung).
+
+abgelegt(clara, einfuehrungspraktium,  1.0).
+abgelegt(clara, einfuehrung_in_die_mathematik_fuer_informatiker, 1.0).
+abgelegt(clara, mathematische_methoden_fuer_informatiker, 1.0).
+abgelegt(clara, algorithmen_und_datenstrukturen, 1.0).
+abgelegt(clara, programmierung, 1.0).
+abgelegt(clara, informtaions_und_kodierungstheorie, 1.0).
+abgelegt(clara, formale_systeme, 1.0).
+abgelegt(clara, theoretische_informatik_und_logik, 1.0).
+abgelegt(clara, softwaretechnologie, 1.0).
+abgelegt(clara, softwaretechnologie_projekt, 1.0).
+abgelegt(clara, rechnerarchitektur, 1.0).
+abgelegt(clara, datenbanken_und_rechnernetze, 1.0).
+abgelegt(clara, betriebssysteme_und_sicherheit, 1.0).
+abgelegt(clara, technische_grundlagen_und_hardware_praktikum, 1.0).
+abgelegt(clara, systemorientierte_informatik_und_hardware_software_codedesign, 1.0).
+abgelegt(clara, intelligente_systeme, 1.0).
+abgelegt(clara, einfuehrung_in_die_medieninformatik, 1.0).
+abgelegt(clara, einfuehrung_in_die_computergraphik, 1.0).
+abgelegt(clara, vertiefung, 1.0).
+abgelegt(clara, vertiefung_zur_bachelor_arbeit, 1.0).
+abgelegt(clara, algemeine_qualifikationen, 1.0).
+abgelegt(clara, bachelorarbeit, 1.0).
+abgelegt(clara, kolloquium, 1.0).
+
+abgelegt(claus, einfuehrungspraktium,  2.0).
+abgelegt(claus, einfuehrung_in_die_mathematik_fuer_informatiker, 2.0).
+abgelegt(claus, algorithmen_und_datenstrukturen, 2.0).
+abgelegt(claus, einfuehrung_in_die_medieninformatik, 2.0).
+abgelegt(clara, programmierung, 5.0).
+abgelegt(clara, informtaions_und_kodierungstheorie, 5.0).
+abgelegt(claus, mathe2, 5.0).
+
+abgelegt(clemens, einfuehrungspraktium,  2.0).
+abgelegt(clemens, einfuehrung_in_die_mathematik_fuer_informatiker, 2.0).
+abgelegt(clemens, algorithmen_und_datenstrukturen, 2.0).
+abgelegt(clemens, einfuehrung_in_die_medieninformatik, 2.0).
+abgelegt(clemens, programmierung, 5.0).
+abgelegt(clemens, informtaions_und_kodierungstheorie, 5.0).
+
+abgelegt(claudia, einfuehrungspraktium,  2.0).
+abgelegt(claudia, einfuehrung_in_die_mathematik_fuer_informatiker, 2.0).
+abgelegt(claudia, algorithmen_und_datenstrukturen, 2.0).
+abgelegt(claudia, einfuehrung_in_die_medieninformatik, 2.0).
+abgelegt(claudia, programmierung, 5.0).
+abgelegt(claudia, informtaions_und_kodierungstheorie, 5.0).
+abgelegt(claudia, mathe1, 5.0).
+abgelegt(claudia, mathe2, 5.0).
+
+exmatrikuliert(clemens).
+
+hat_mehrere_pruefungen(mathematische_methoden_fuer_informatiker, [mathe1, mathe2, ende]).
 
 
-
-
-alle_module_bestanden(X,[Head|Tail]) :-
+/*REGELN:*/
+/*hilfsregeln*/
+alle_module_bestanden(X, [Head|Tail]) :-
 	(Head == ende);
-	(abgelegt(Head, X, Note),
+	(abgelegt(X, Head, Note),
 	Note =< 4.0,
-	alle_module_bestanden(X,Tail)).
-
-verbesserungsversuch(X,Y) :- 
-	wurde_verbessert(X,Y,nein).
+	alle_module_bestanden(X, Tail)).
 
 allg_zugelassen(X) :-
 	studienzeit_nicht_ueberschritten(X),
 	eingeschrieben(X, bachelor_informatik, tu_dresden),
-	erklaerung(X).
+	erklaerung(X),
+	\+exmatrikuliert(X).
 
+/*regeln nach Aufgabenstellung*/
 pruefungszulassung(X,Y) :-
 	allg_zugelassen(X),
 	angemeldet(X,Y),
 	offen(X,Y,_).
-	/*(freiversuch(X,Y);
-	erstpruefung(X,Y);
-	erste_wiederholung(X,Y);
-	zweite_wiederholung(X,Y);
-	verbesserungsversuch(X,Y)).*/
+
+pruefung_bestanden(X, Y) :-
+	abgelegt(X, Y, Note),
+	Note =< 4.0.
 
 zulassung_bachelorarbeit(X) :-
 	allg_zugelassen(X),
-	antrag_ausgabe_bachelorthema(X),
-	(b_erstpruefung(X);
-	b_erste_wiederholung(X)).
+	angemeldet(X, bachelorarbeit),
+	offen(X, bachelorarbeit, _).
 
 zulassung_kolloquium(X) :-
 	allg_zugelassen(X),
-	bachelorarbeit(X, Note),
+	abgelegt(X, bachelorarbeit, Note),
 	Note =< 4.0.
 
 abschluss_erhalten(X) :-
-	bachelorarbeit(X, Bnote),
+	abgelegt(X, bachelorarbeit, Bnote),
 	Bnote =< 4.0,
-	kolloquium(X, Knote),
+	abgelegt(X, kolloquium, Knote),
 	Knote =< 4.0,
-	alle_module_bestanden(X, [tgi,ra,mathe, ende]).
+	alle_module_bestanden(X, [einfuehrungspraktium, 
+														einfuehrung_in_die_mathematik_fuer_informatiker,
+														mathematische_methoden_fuer_informatiker,
+														algorithmen_und_datenstrukturen,
+														programmierung,
+														informtaions_und_kodierungstheorie,
+														formale_systeme,
+														theoretische_informatik_und_logik,
+														softwaretechnologie,
+														softwaretechnologie_projekt,
+														rechnerarchitektur,
+														datenbanken_und_rechnernetze,
+														betriebssysteme_und_sicherheit,
+														technische_grundlagen_und_hardware_praktikum,
+														systemorientierte_informatik_und_hardware_software_codedesign,
+														intelligente_systeme,
+														einfuehrung_in_die_medieninformatik,
+														einfuehrung_in_die_computergraphik,
+														vertiefung,
+														vertiefung_zur_bachelor_arbeit,
+														algemeine_qualifikationen,
+														ende]).
+
+/*zusatzregeln:*/
+ist_exmatrikuliert(X) :-
+	exmatrikuliert(X).
+
+/*Aufrufen zum Testen von der dritten Aufgabe, mit claus oder claudia. Wenn man weniger schreiben will.*/
+aufgabe_drei(X) :-
+	mehrere_wiederholungen_testen(X, mathematische_methoden_fuer_informatiker).
+
+mehrere_wiederholungen_testen(X, Y) :-
+	hat_mehrere_pruefungen(Y, Liste_Pruefungen),
+	pruefungen_testen(X, Liste_Pruefungen, 0.0).
+
+pruefungen_testen(X, [Head|Tail], Anzahl) :-
+	(Head == ende,
+	Anzahl =< 1.0);
+	(offen(X, Head, Versuch),
+	Versuch == zweite_wiederholung,
+	Banzahl is Anzahl +1,
+	pruefungen_testen(X, Tail, Banzahl)).
+
+pruefungen_testen(X, [Head|Tail], Anzahl) :-
+	(Head == ende,
+	Anzahl =< 1.0);
+	(offen(X, Head, Versuch),
+	Versuch \= zweite_wiederholung,
+	pruefungen_testen(X, Tail, Anzahl)).
+
+
+
 
 
 	
